72eedd32c76178c395522eda6f28f52a2d8ed263
Valerii Pimenov <valeriipimenov@MacBook-Air-admin.local>
2020-07-15 10:19:55 +0300

    Updates

---
+++ precache-manifest.0fbb19875875cfefcc542d42dce979c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb6aea4407d387014c4897360bf4ae4",
+    "url": "/task-menager/index.html"
+  },
+  {
+    "revision": "ba306d0eacb1f38c3ba4",
+    "url": "/task-menager/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "e56f296befb8777825fa",
+    "url": "/task-menager/static/css/main.d0f015fb.chunk.css"
+  },
+  {
+    "revision": "ba306d0eacb1f38c3ba4",
+    "url": "/task-menager/static/js/2.84a1ed88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/task-menager/static/js/2.84a1ed88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56f296befb8777825fa",
+    "url": "/task-menager/static/js/main.edac5c97.chunk.js"
+  },
+  {
+    "revision": "98c925ba70f66a14d2a1",
+    "url": "/task-menager/static/js/runtime-main.e1c601ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b9a8ace30dd0f4243d565c84b78f2e4.js"
+  "/task-menager/precache-manifest.0fbb19875875cfefcc542d42dce979c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/task-menager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
